181d26d613f22e06f6015f440189a4fd1100f314
Riku Choy <rikuchoy@gmail.com>
2023-06-11 17:31:15 -0700

    imported required files into index.js, wrote prompts for users

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {Circle, Square, Triangle} = require('./lib/shapes');
+
+function userPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter logo text (limited to 3 characters)',
+                name: 'text',
+            },
+            {
+                type: 'input',
+                message: 'Choose your text color (enter color keyword or hex number)',
+                name: 'textColor',
+            },
+            {
+                type: 'list',
+                message: 'Select the shape you would like your logo to be',
+                choices: ['Cirlce', 'Square', 'Triangle'],
+                name: 'shape',
+            },
+            {
+                type: 'input',
+                message: 'Choose your shape color (enter color keyword or hex number)',
+                name: 'shapeColor',
+            },
+        ])
+        .then((answers) => {
+            if (answers.)
+        })
+}
